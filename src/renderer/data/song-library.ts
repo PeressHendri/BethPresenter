2803872c464c3c0ea69b2da6c6c851788ce63d67
Peres Hendri <peres.gaul@gmail.com>
2026-04-12 02:04:53 +0700

    Delete

--- src/renderer/data/song-library.ts
+++
@@ -1,8 +0,0 @@
-export const MOCK_SONG_LIBRARY = [
-  { id: '1', title: 'From the Inside Out', author: 'Hillsong', slides: 6, addedAt: 'Just now', tags: ['Solemn'] },
-  { id: '2', title: 'Here Again', author: 'Elevation Worship', slides: 35, addedAt: '26m ago', tags: ['Solemn'] },
-  { id: '3', title: 'Way Maker', author: 'Sinach', slides: 20, addedAt: '43m ago', tags: ['Joyful'] },
-  { id: '4', title: 'He’s in the Room', author: 'Unknown', slides: 25, addedAt: '1h ago', tags: ['Joyful'] },
-  { id: '5', title: 'Pusong Basag', author: 'Victory Worship', slides: 27, addedAt: '3h ago', tags: ['Solemn'] },
-  { id: '6', title: 'Can’t Stop Praising Him', author: 'Unknown', slides: 15, addedAt: '1d ago', tags: ['Joyful'] },
-];
